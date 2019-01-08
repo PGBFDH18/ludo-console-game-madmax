692cc19c3a7613bedc35ec02727e235f9ef22f1f
Joachim Nordeke <joachim@nordeke.se>
2019-01-08 13:32:37 +0100

    BaseProject

---
+++ src/LudoByMadMax/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LudoByMadMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
