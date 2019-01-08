16eb03090e43dfc5e0e8e39ac0868bced1283bc6
Joachim Nordeke <joachim@nordeke.se>
2019-01-08 15:19:55 +0100

    Update Program.cs

--- src/LudoByMadMax/Program.cs
+++ src/LudoByMadMax/Program.cs
@@ -6,16 +6,19 @@ namespace LudoByMadMax
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Ludo, by MadMax");
-            Console.Write("Enter number of players: ");
+            Console.WriteLine("Welcome to Ludo - by MadMax");
+            Console.WriteLine("---------------------------");
+            Console.Write("Number of players: ");
             int players = int.Parse(Console.ReadLine());
 
-            for (int i = 1; i < players + 1; i++)
+            for (int i = 1; i <= players; i++)
             {
                 Console.Write("Enter the name of Player " + i + ": ");
                 string name = Console.ReadLine();
                 // create new Player-object. Player.Name = name
             }
+
+
         }
     }
 }
